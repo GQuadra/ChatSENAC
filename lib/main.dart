d0513253b1879b4b2b2876e8567a18a14c2427e8
Gabriel Quadra <gabriel.quadra@edu.es.senac.br>
2026-07-24 21:41:22 -0300

    cria pasta de assets
    - adiciona a fonte Inter
    - aplica um espaçamento entre a logo e os titulos

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class Login extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Row(children: [FlutterLogo(size: 18), Text("ChatSENAC")]),
+        SizedBox(height: 32,),
 
         // Titulos
         Text("Entre na sua Conta", style: Tipografia.h1),
