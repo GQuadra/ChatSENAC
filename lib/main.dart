8d1579cc6ab68c669096263b9fc47c9e6c0c9e1b
Gabriel Quadra <gabriel.quadra@edu.es.senac.br>
2026-07-29 21:26:02 -0300

    cria pagina de cadastro
    - cria uma pagina de cadastro com os campos Nome, Email, Senha e Confirmar Senha
    - monta apenas o design da pagina

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_app/paginas/cadastro.dart';
 import 'package:primeiro_app/paginas/login.dart';
 
 void main() {
@@ -6,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
